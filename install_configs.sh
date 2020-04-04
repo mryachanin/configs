2a14d0c330efae56c216f905a984173e80d4d8d6
Mike Yachanin <michael@yachanin.com>
2020-04-04 04:16:12 -0700

    [gitconfig] Set autocrlf = input

--- install_configs.sh
+++ install_configs.sh
@@ -2,6 +2,8 @@
 
 cd $(dirname $0)
 
+# For every file defined in the configs project, create a folder in the
+# current user's home directory and symlink any files respectively.
 for file in $(find configs -type f);
 do
   config_dir=$(dirname ${file:8})
