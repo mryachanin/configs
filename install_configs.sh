a9af33e0e2e05bb57a78a8f83123e5a68d63ab44
Mike Yachanin <michael@yachanin.com>
2020-04-04 04:16:39 -0700

    [install_configs] Automatically copy git config_local

--- install_configs.sh
+++ install_configs.sh
@@ -10,3 +10,13 @@ do
   mkdir -p $HOME/$config_dir
   ln -sf $(pwd)/$file $HOME/$config_dir/
 done
+
+# Install a local gitconfig file that is used for properties such as name and
+# email address.
+# Also cleans up the sample file post-copy as it is explicitly copied over to
+# the correct path.
+# Note: Git includes do not follow symlinks.
+file=$(find configs -type f -name config_local.sample)
+config_dir=$(dirname ${file:8})
+cp $(pwd)/$file $HOME/$config_dir/config_local
+rm $HOME/$config_dir/config_local.sample
