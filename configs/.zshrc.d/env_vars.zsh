a7e7c27c41d01a39c6bc2fbf850fc8669dd4b53f
Mike Yachanin <michael@yachanin.com>
2020-06-21 04:30:22 -0700

    Integrate zsh shell with git

--- configs/.zshrc.d/env_vars.zsh
+++ configs/.zshrc.d/env_vars.zsh
@@ -11,3 +11,13 @@ export VISUAL=$EDITOR
 
 # Set prompt to '[username@hostname:dir]% '
 export PROMPT='[%n@%m:%c]%# '
+
+# Enable git integration with right-hand side prompt
+autoload -Uz vcs_info
+precmd_vcs_info() { vcs_info }
+precmd_functions+=( precmd_vcs_info )
+setopt prompt_subst
+RPROMPT=\$vcs_info_msg_0_
+zstyle ':vcs_info:git:*' formats '%F{240}(%b)%r%f'
+zstyle ':vcs_info:*' enable git
+
